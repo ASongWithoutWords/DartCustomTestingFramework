f28b2492b55708d116fa6f8b92cb5fe30d0920f7
Ian <ianmccall93@gmail.com>
2015-09-24 00:01:05 -0700

    Revised sample usage and added screenshot of sample output.

--- dartTest/test+anomaly.dart
+++ dartTest/test+anomaly.dart
@@ -1,49 +1,47 @@
-
 part of dartTest;
 
-class Test extends _Ledger<_Result>{
+class Test extends _Ledger<_Result> {
+  static Test _currentTest;
 
-	static Test _currentTest;
+  static void _addEntry(_Result result) => _currentTest._entries.add(result);
 
-	static void _addEntry(_Result result) => _currentTest._entries.add(result);
+  Test(String name, Function testBody) : super(name, []) {
+    _currentTest = this;
+    testBody();
+  }
 
-
-	Test(String name, Function testBody) : super(name, []){
-		_currentTest = this;
-		testBody();
-	}
-
-	/// Generates an HTML node that can be appended to the node of a ledger.
-	UListElement get _node{
-		var node = new UListElement()
-			..style.marginLeft = '1em'
-			..style.paddingLeft = '0em'
-			..style.color = _passed? 'LimeGreen' : 'Red';
+  /// Generates an HTML node that can be appended to the node of a ledger.
+  UListElement get _node {
+    var node = new UListElement()
+      ..style.marginLeft = '1em'
+      ..style.paddingLeft = '0em'
+      ..style.color = _passed ? 'LimeGreen' : 'Red';
     // Generates and appends either a green checkmark or a red x.
-		var bullet = new SpanElement()
-			..style.fontFamily = 'Webdings'
-			..text = _passed? 'a' : 'r';
-		node.append(bullet);
-		node.appendText(' '+_name);
+    var bullet = new SpanElement()
+      ..style.fontFamily = 'Webdings'
+      ..text = _passed ? 'a' : 'r';
+    node.append(bullet);
+    node.appendText(' ' + _name);
 
-    _entries.forEach( (result) => node.append(result._node) );
+    _entries.forEach((result) => node.append(result._node));
 
-		return node;
-	}
+    return node;
+  }
 }
 
-class _Result implements _Entry{
-	bool get _passed => false;
-	String _message = '';
-	_Result(this._message);
+class _Result implements _Entry {
+  bool get _passed => false;
+  String _message = '';
+  _Result(this._message);
+
   /// Generates an HTML node that can be appended to the node of a test.
-	LIElement get _node{
-		if(_message.isEmpty) return null;
-		var node = new LIElement()
-			..style.textIndent = '0em'
-			..style.marginLeft = '2em'
-			..style.color = 'red'
-			..text = this._message;
-		return node;
-	}
+  LIElement get _node {
+    if (_message.isEmpty) return null;
+    var node = new LIElement()
+      ..style.textIndent = '0em'
+      ..style.marginLeft = '2em'
+      ..style.color = 'red'
+      ..text = this._message;
+    return node;
+  }
 }
--- sampleUsage.dart
+++ sampleUsage.dart
@@ -1,50 +1,35 @@
 library sampleUsage;
 
-import 'dart:math';
 import './dartTest/dartTest.dart';
 
-// Example cat and dog classes for failing test.
-class Cat {}
-class Dog {}
-
-// Example vector class to test our testing framework.
-class Vector {
-  final num x, y;
-  Vector(this.x, this.y);
-
-  // Returns the magnitude of this vector.
-  num get magnitude => sqrt(x * x + y * y);
-
-  // Returns true if the vectors are equal and false otherwise.
-  bool operator ==(Vector b) => (x == b.x) && (y == b.y);
-
-  // Returns a new vector that is the sum of the two vectors.
-  Vector operator +(Vector b) => new Vector(x + b.x, y + b.y);
+class Sky {
+  static bool get isBlue => true;
 }
 
+class Dog {
+  static bool get isMansBestFriend => true;
+}
+
+class Cat {
+  static num get numLives => 9;
+}
 
 main() {
   new Module('Sample Usage', [
-    new Group('Vector:', [
-      new Test('Equality test', () {
-        var v = new Vector(7, 4);
-        var u = new Vector(7, 4);
-        expect.areEqual(u, v);
+    new Group('Test Group A:', [
+      new Test('The sky is blue', () {
+        expect.isTrue(Sky.isBlue);
       }),
-      new Test('Addition test', () {
-        var v = new Vector(1, 2);
-        var u = new Vector(7, 5);
-        var w = new Vector(8, 7);
-        expect.areEqual(u + v, w);
-      }),
-      new Test('Magnitude test', () {
-        var v = new Vector(3, 4);
-        expect.areEqual(v.magnitude, 5);
-      })
-    ]),
-    new Group('Failing Tests:', [
       new Test('Yes means no', () {
         expect.areEqual('yes', 'no');
+      }),
+      new Test('A dog is a man\'s best friend', () {
+        expect.isTrue(Dog.isMansBestFriend);
+      }),
+    ]),
+    new Group('Test Group B:', [
+      new Test('A cat has nine lives', () {
+        expect.areEqual(Cat.numLives, 9);
       }),
       new Test('My cat is a dog', () {
         var myCat = new Cat();
